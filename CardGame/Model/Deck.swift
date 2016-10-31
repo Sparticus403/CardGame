a97a2deb2f82f50e53bb5d2efe7b07ff99584d11
Sparticus403 <twil8980@740s-pl-332.local>
2016-10-31 08:13:00 -0600

    Created new class; deck

---
+++ CardGame/Model/Deck.swift
@@ -0,0 +1,14 @@
+//
+//  Deck.swift
+//  CardGame
+//
+//  Created by Williamson, Todd on 10/31/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class Deck : PlayingCard
+{
+    
+}
