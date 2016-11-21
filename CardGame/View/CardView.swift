4ebbb01132c0e61f87768fa905c679b72d39f137
Sparticus403 <twil8980@740s-pl-388.local>
2016-11-21 09:55:22 -0700

    initialized the internal methods

---
+++ CardGame/View/CardView.swift
@@ -0,0 +1,34 @@
+//
+//  CardView.swift
+//  CardGame
+//
+//  Created by Williamson, Todd on 11/21/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class CardView
+{
+    internal var isFaceUp : Bool
+    internal var cardBack : UIImageView
+    internal var cardFront : UIImageView
+    internal var topLabel : UILabel
+    internal var bottomLabel : UILabel
+    internal var cornerCurve : CGFloat
+    
+    init()
+    {
+        isFaceUp = false
+        cardBack = UIImageView()
+        cardFront = UIImageView()
+        topLabel = UILabel()
+        bottomLabel = UILabel()
+        cornerCurve = CGFloat (20.00)
+    }
+    
+    func setup() -> Void
+    {
+        
+    }
+}
